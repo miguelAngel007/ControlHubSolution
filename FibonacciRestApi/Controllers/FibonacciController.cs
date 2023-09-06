3ccd4f75145e7cb62fcab11dca9907e6c9df236b
bones <bones@BONES-PC>
2023-09-06 17:47:32 -0500

    Add project files.

---
+++ FibonacciRestApi/Controllers/FibonacciController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FibonacciRestApi.Controllers
+{
+    [ApiController]
+    [Route("controlhub/api/[controller]")]
+    public class fibonacciController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult calc(int index)
+        {
+            try
+            {
+                List<int> serie = getFibonacciSerie(index);
+                int value = serie[index];
+
+                return Ok(new {serie,index,value});
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        private List<int> getFibonacciSerie(int index)
+        {
+
+            int n = index<= 8 ? 8 : 8 + index;
+
+            int a = 0; 
+            int b = 1;
+
+            List<int> serie = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                serie.Add(a);
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return serie;
+        }
+
+    }
+}
---
+++ MostFrecuentWord/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //book is a fragment from a bojack serie in netflix
+        string book = @"Horsin' Around is filmed
+                        before a live studio audience.
+                        Mondays.
+                        Well, good morning to you too.
+                        Oh, hey.
+                        Where? I'd love hay.
+                        In 1987, the situation
+                        comedy Horsin' Around
+                        premiered on ABC.
+                        The show, in which a young, bachelor horse
+                        is forced to reevaluate his priorities
+                        when he agrees to raise
+                        three human children,
+                        was initially dismissed by critics
+                        as broad and saccharine and not good,
+                        but the family comedy
+                        struck a chord with America
+                        and went on to air for nine seasons.
+                        The star of Horsin'
+                        Around, BoJack Horseman,
+                        is our guest tonight.
+                        Welcome, BoJack.";
+
+        // Tokenize the text by splitting it into words
+        string[] excludes = { "\n","\r", "\r\n", "\n\r" };//remove new line
+        string[] words = book.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Where(x=> !excludes.Contains(x)).ToArray();
+
+        // Create a dictionary to store word frequencies
+        Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+
+        // Count the frequency of each word
+        foreach (string word in words)
+        {
+            string cleanedWord = word.ToLower(); // Convert to lowercase for case-insensitive counting
+            if (wordFreq.ContainsKey(cleanedWord))
+                wordFreq[cleanedWord]++;
+            else
+                wordFreq[cleanedWord] = 1;
+        }
+
+        // Find the most frequent word
+        KeyValuePair<string, int> mostFrequentWord = wordFreq.OrderByDescending(pair => pair.Value).First();
+        string output = $"The most frequent word is '{mostFrequentWord.Key}' with a frequency of {mostFrequentWord.Value} times.";
+        Console.WriteLine(output);
+    }
+}
